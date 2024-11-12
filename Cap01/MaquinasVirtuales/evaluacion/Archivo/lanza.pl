a6f8284d0bc751a2874ece2a0d0af645bc870a9c
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2024-11-12 01:59:39 -0500

    Update lanza.pl

--- Cap01/MaquinasVirtuales/evaluacion/Archivo/lanza.pl
+++ Cap01/MaquinasVirtuales/evaluacion/Archivo/lanza.pl
@@ -8,22 +8,37 @@
 #     Fichero: script automatización ejecución por lotes 
 #****************************************************************/
 
+# Obtener el directorio actual y eliminar el salto de línea
 $Path = `pwd`;
-chomp($Path);
+chomp($Path); 
 
-$Nombre_Ejecutable = "MM_ejecutable";
-@Size_Matriz = ("200","300");
-@Num_Hilos = (1,2);
-$Repeticiones = 2;
+# Nombre del ejecutable que se va a ejecutar
+$Nombre_Ejecutable = "MM_ejecutable"; 
 
-foreach $size (@Size_Matriz){
-	foreach $hilo (@Num_Hilos) {
-		$file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
-		for ($i=0; $i<$Repeticiones; $i++) {
-#system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
-			printf("$Path/$Nombre_Ejecutable $size $hilo \n");
-		}
-		close($file);
-	$p=$p+1;
-	}
+# Tamaños de las matrices a utilizar en las pruebas
+@Size_Matriz = ("200","300"); 
+
+# Número de hilos a utilizar en las pruebas
+@Num_Hilos = (1,2); 
+
+# Número de repeticiones para cada combinación de tamaño y hilos
+$Repeticiones = 2; 
+
+# Iterar sobre cada tamaño de matriz
+foreach $size (@Size_Matriz) {
+    # Iterar sobre cada número de hilos
+    foreach $hilo (@Num_Hilos) {
+        # Crear el nombre del archivo de salida basado en el tamaño y número de hilos
+        $file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat"; 
+        
+        # Repetir la ejecución del programa para cada combinación
+        for ($i=0; $i<$Repeticiones; $i++) {
+            # Ejecutar el programa con los parámetros correspondientes y redirigir la salida al archivo
+            system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
+            # Imprimir el comando que se ejecutaría (actualmente comentado)
+            printf("$Path/$Nombre_Ejecutable $size $hilo \n"); 
+        }
+        close($file); # Cerrar el archivo (no es necesario aquí, ya que no se abrió con 'open')
+        $p=$p+1; # Contador de ejecuciones (no se utiliza en el resto del script)
+    }
 }
